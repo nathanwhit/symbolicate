cf3fddd2c0c34a9ae2025284191a5ac5e723675a
Nathan Whitaker <nathan@deno.com>
2025-02-28 16:26:00 -0800

    tailwind

--- js/client-side/vite.config.ts
+++ js/client-side/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import deno from "@deno/vite-plugin";
 import preact from "@preact/preset-vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [deno(), ...preact()],
+  plugins: [deno(), preact(), tailwindcss()],
   server: {
     port: 3000,
   },
